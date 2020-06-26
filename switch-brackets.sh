f1693616dfe29a1be3f2b963612e7b2624aa3505
prrdee <18732242+prrdee@users.noreply.github.com>
2020-06-26 20:55:29 +0300

    change 34 and 26

--- switch-brackets.sh
+++ switch-brackets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A simple script to map 8 and 9 keys to ö and ä so that it is easier to type brackets
 
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000033,"HIDKeyboardModifierMappingDst":0x700000025},{"HIDKeyboardModifierMappingSrc":0x700000025,"HIDKeyboardModifierMappingDst":0x700000033}]}'
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000033,"HIDKeyboardModifierMappingDst":0x700000025},{"HIDKeyboardModifierMappingSrc":0x700000034,"HIDKeyboardModifierMappingDst":0x700000026}]}'
